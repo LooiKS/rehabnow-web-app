d5997a7d4e2b9074b175ebc9bf6494007a83a65d
Kian Seng Looi <kianseng213@gmail.com>
2021-06-09 00:42:00 +0800

    change py version

--- rehabnow/settings.py
+++ rehabnow/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATIC_URL = "/static/"
 
